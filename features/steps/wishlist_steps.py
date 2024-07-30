c61aff37e355b7bd12f72e7435d7e9144d9b3913
Tzu An Wang <zianwang.wayne@gmail.com>
2024-07-30 20:31:26 +0000

    update wishlist_steps

--- features/steps/wishlist_steps.py
+++ features/steps/wishlist_steps.py
@@ -1,13 +1,74 @@
-import os
+# pylint: disable=function-redefined
+# flake8: noqa
+"""
+Wishlist Steps
+
+Steps file for Wishlists.feature
+
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
 import requests
-from behave import given, when, then
+from compare3 import expect
+from behave import given  # pylint: disable=no-name-in-module
 
-@given('the server is started')
+# HTTP Return Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
+
+WAIT_TIMEOUT = 60
+
+
+@given('the following wishlists')
 def step_impl(context):
-    context.base_url = os.getenv(
-        'BASE_URL',
-        'http://localhost:8080'
-    )
+    """ Delete all Wishlists and load new ones """
 
-    context.resp = requests.get(context.base_url + '/')
-    assert context.resp.status_code == 200
+    # Get a list all of the wishlists
+    rest_endpoint = f"{context.base_url}/shopcarts"
+    context.resp = requests.get(rest_endpoint, timeout=WAIT_TIMEOUT)
+    expect(context.resp.status_code).equal_to(HTTP_200_OK)
+    # and delete them one by one
+    for wishlist in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{wishlist['id']}", timeout=WAIT_TIMEOUT)
+        expect(context.resp.status_code).equal_to(HTTP_204_NO_CONTENT)
+
+    # load the database with new wishlists
+    for row in context.table:
+        payload = {
+            "customer_id": float(row['customer_id']),
+            "name": float(row['name'])
+        }
+        context.resp = requests.post(rest_endpoint, json=payload, timeout=WAIT_TIMEOUT)
+        expect(context.resp.status_code).equal_to(HTTP_201_CREATED)
+
+
+@given('the following wishlist items')
+def step_impl(context):
+    """ Delete all Wishlist Items and load new ones """
+
+    # Get a list all of the wishlists
+    rest_endpoint = f"{context.base_url}/wishlists"
+    context.resp = requests.get(rest_endpoint, timeout=WAIT_TIMEOUT)
+    assert context.resp.status_code == HTTP_200_OK
+    # and delete all wishlist items in the wishlists one by one
+    wishlist_ids = []
+    for wishlist in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{wishlist['id']}/items", timeout=WAIT_TIMEOUT)
+        expect(context.resp.status_code).equal_to(HTTP_204_NO_CONTENT)
+        wishlist_ids.append(wishlist["id"])
+
+    # Load the database with new wishlist items
+    wishlist_index = 0  # Initialize index to assign wishlists
+    for row in context.table:
+        wishlist_id = wishlist_ids[wishlist_index]  # Get the wishlist ID by index
+        payload = {
+            "wishlist_id": wishlist_id,
+            "product_id": int(row["product_id"]),
+            "description": row["description"],
+            "price": float(row["price"])
+        }
+        context.resp = requests.post(f"{rest_endpoint}/{wishlist_id}/items", json=payload, timeout=WAIT_TIMEOUT)
+        assert context.resp.status_code == HTTP_201_CREATED
+        # Move to the next wishlist after assigning an items to the current wishlist
+        wishlist_index = wishlist_index + 1
