3083e70a6e3f7538e8463eb6efb4d8c09dc7be99
Tzu An Wang <zianwang.wayne@gmail.com>
2024-07-30 18:59:51 +0000

    update source page ui

--- features/wishlists.feature
+++ features/wishlists.feature
@@ -1,7 +1,7 @@
 Feature: The wishlist store service back-end
     As a Wishlist Store Owner
     I need a RESTful catalog service
-    So that I can keep track of all my wishlists
+    So that I can keep track of all the wishlists and wishlist items
 
 Background:
 
@@ -9,3 +9,19 @@ Scenario: The server is running
     When I visit the "home page"
     Then I should see "Wishlist Demo RESTful Service" in the title
     And  I should not see "404 Not Found"
+
+Scenario: Create a Wishlist
+    When I visit the "Home Page"
+    And I set the "Shopcart total price" to "10"
+    And I press the "Shopcart Create" button
+    Then I should see the message "Shopcart has been Created!"
+    When I copy the "Shopcart ID" field
+    And I press the "Shopcart Form Clear" button
+    Then the "Shopcart ID" field should be empty
+    And the "Shopcart Item product ID" field should be empty
+    And the "Shopcart Item name" field should be empty
+    And the "Shopcart total price" field should be empty
+    When I paste the "Shopcart Id" field
+    And I press the "Shopcart Retrieve" button
+    Then I should see the message "Success"
+    And I should see "10" in the "Shopcart total price" field
--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -4,27 +4,40 @@ $(function () {
     //  U T I L I T Y   F U N C T I O N S
     // ****************************************
 
-    // Updates the form with data from the response
-    function update_form_data(res) {
-        $("#pet_id").val(res.id);
-        $("#pet_name").val(res.name);
-        $("#pet_category").val(res.category);
-        if (res.available == true) {
-            $("#pet_available").val("true");
-        } else {
-            $("#pet_available").val("false");
-        }
-        $("#pet_gender").val(res.gender);
-        $("#pet_birthday").val(res.birthday);
+    // Updates the wishlist form with data from the response
+    function update_wishlist_form_data(res) {
+        $("#wishlist_id").val(res.id);
+        $("#wishlist_name").val(res.name);
+        $("#wishlist_item_product_id").val(res.item_product_id);
+        $("#wishlist_item_name").val(res.item_name);
     }
 
-    /// Clears all form fields
-    function clear_form_data() {
-        $("#pet_name").val("");
-        $("#pet_category").val("");
-        $("#pet_available").val("");
-        $("#pet_gender").val("");
-        $("#pet_birthday").val("");
+    // Updates the wishlist item form with data from the response
+    function update_wishlist_form_data(res) {
+        $("#item_id").val(res.id);
+        $("#item_wishlist_id").val(res.wishlist_id);
+        $("#item_product_id").val(res.product_id);
+        $("#item_name").val(res.name);
+        $("#item_description").val(res.description);
+        $("#item_price").val(res.price);
+    }
+
+    /// Clears all wishlist form fields
+    function clear_wishlist_form_data() {
+        $("#wishlist_id").val("");
+        $("#wishlist_name").val("");
+        $("#wishlist_item_product_id").val("");
+        $("#wishlist_item_name").val("");
+    }
+
+    /// Clears all wishlist item form fields
+    function clear_wishlist_item_form_data() {
+        $("#item_id").val("");
+        $("#item_wishlist_id").val("");
+        $("#item_product_id").val("");
+        $("#item_name").val("");
+        $("#item_description").val("");
+        $("#item_price").val("");
     }
 
     // Updates the flash message area
@@ -34,7 +47,12 @@ $(function () {
     }
 
     // ****************************************
-    // Create a Pet
+    //  W I S H L I S T   F U N C T I O N S
+    // ****************************************
+
+
+    // ****************************************
+    // Create a Wishlist
     // ****************************************
 
     $("#create-btn").click(function () {
@@ -74,7 +92,13 @@ $(function () {
 
 
     // ****************************************
-    // Update a Pet
+    // List Wishlists
+    // ****************************************
+
+
+
+    // ****************************************
+    // Update a Wishlist
     // ****************************************
 
     $("#update-btn").click(function () {
@@ -115,7 +139,7 @@ $(function () {
     });
 
     // ****************************************
-    // Retrieve a Pet
+    // Retrieve a Wishlist
     // ****************************************
 
     $("#retrieve-btn").click(function () {
@@ -145,7 +169,7 @@ $(function () {
     });
 
     // ****************************************
-    // Delete a Pet
+    // Delete a Wishlist
     // ****************************************
 
     $("#delete-btn").click(function () {
@@ -172,17 +196,7 @@ $(function () {
     });
 
     // ****************************************
-    // Clear the form
-    // ****************************************
-
-    $("#clear-btn").click(function () {
-        $("#pet_id").val("");
-        $("#flash_message").empty();
-        clear_form_data()
-    });
-
-    // ****************************************
-    // Search for a Pet
+    // Search Wishlists
     // ****************************************
 
     $("#search-btn").click(function () {
@@ -257,4 +271,255 @@ $(function () {
 
     });
 
+    // ****************************************
+    // Clear the Wishlist form
+    // ****************************************
+
+    $("#wishlist-form-clear-btn").click(function () {
+        $("#flash_message").empty();
+        clear_wishlist_form_data()
+    });
+
+
+    // *************************************************
+    //  W I S H L I S T  I T E M   F U N C T I O N S
+    // *************************************************
+
+
+    // ****************************************
+    // Create a Wishlist Item
+    // ****************************************
+
+    $("#create-btn").click(function () {
+
+        let name = $("#pet_name").val();
+        let category = $("#pet_category").val();
+        let available = $("#pet_available").val() == "true";
+        let gender = $("#pet_gender").val();
+        let birthday = $("#pet_birthday").val();
+
+        let data = {
+            "name": name,
+            "category": category,
+            "available": available,
+            "gender": gender,
+            "birthday": birthday
+        };
+
+        $("#flash_message").empty();
+        
+        let ajax = $.ajax({
+            type: "POST",
+            url: "/pets",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+        });
+
+        ajax.done(function(res){
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+    });
+
+
+    // ****************************************
+    // List Wishlist Items
+    // ****************************************
+
+
+
+    // ****************************************
+    // Update a Wishlist Item
+    // ****************************************
+
+    $("#update-btn").click(function () {
+
+        let pet_id = $("#pet_id").val();
+        let name = $("#pet_name").val();
+        let category = $("#pet_category").val();
+        let available = $("#pet_available").val() == "true";
+        let gender = $("#pet_gender").val();
+        let birthday = $("#pet_birthday").val();
+
+        let data = {
+            "name": name,
+            "category": category,
+            "available": available,
+            "gender": gender,
+            "birthday": birthday
+        };
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+                type: "PUT",
+                url: `/pets/${pet_id}`,
+                contentType: "application/json",
+                data: JSON.stringify(data)
+            })
+
+        ajax.done(function(res){
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Retrieve a Wishlist Item
+    // ****************************************
+
+    $("#retrieve-btn").click(function () {
+
+        let pet_id = $("#pet_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/pets/${pet_id}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            clear_form_data()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Delete a Wishlist Item
+    // ****************************************
+
+    $("#delete-btn").click(function () {
+
+        let pet_id = $("#pet_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "DELETE",
+            url: `/pets/${pet_id}`,
+            contentType: "application/json",
+            data: '',
+        })
+
+        ajax.done(function(res){
+            clear_form_data()
+            flash_message("Pet has been Deleted!")
+        });
+
+        ajax.fail(function(res){
+            flash_message("Server error!")
+        });
+    });
+
+    // ****************************************
+    // Search Wishlist Items
+    // ****************************************
+
+    $("#search-btn").click(function () {
+
+        let name = $("#pet_name").val();
+        let category = $("#pet_category").val();
+        let available = $("#pet_available").val() == "true";
+
+        let queryString = ""
+
+        if (name) {
+            queryString += 'name=' + name
+        }
+        if (category) {
+            if (queryString.length > 0) {
+                queryString += '&category=' + category
+            } else {
+                queryString += 'category=' + category
+            }
+        }
+        if (available) {
+            if (queryString.length > 0) {
+                queryString += '&available=' + available
+            } else {
+                queryString += 'available=' + available
+            }
+        }
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/pets?${queryString}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            $("#search_results").empty();
+            let table = '<table class="table table-striped" cellpadding="10">'
+            table += '<thead><tr>'
+            table += '<th class="col-md-2">ID</th>'
+            table += '<th class="col-md-2">Name</th>'
+            table += '<th class="col-md-2">Category</th>'
+            table += '<th class="col-md-2">Available</th>'
+            table += '<th class="col-md-2">Gender</th>'
+            table += '<th class="col-md-2">Birthday</th>'
+            table += '</tr></thead><tbody>'
+            let firstPet = "";
+            for(let i = 0; i < res.length; i++) {
+                let pet = res[i];
+                table +=  `<tr id="row_${i}"><td>${pet.id}</td><td>${pet.name}</td><td>${pet.category}</td><td>${pet.available}</td><td>${pet.gender}</td><td>${pet.birthday}</td></tr>`;
+                if (i == 0) {
+                    firstPet = pet;
+                }
+            }
+            table += '</tbody></table>';
+            $("#search_results").append(table);
+
+            // copy the first result to the form
+            if (firstPet != "") {
+                update_form_data(firstPet)
+            }
+
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Clear the Wishlist Item form
+    // ****************************************
+
+    $("#item-form-clear-btn").click(function () {
+        $("#flash_message").empty();
+        clear_wishlist_item_form_data()
+    });
+
+
 })
+
+
+
+
+
+
